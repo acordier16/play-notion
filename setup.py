43829e076e0fbe5718174bc4c0cffe3c255c65eb
acordier <acordier$@scortex.io>
2023-08-07 10:47:12 +0200

    setup.py: add termcolor, set version to 1.0

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="play-notion",
-    version="0.1",
+    version="1.0",
     packages=find_packages(),
-    install_requires=["requests>=2.31.0"],
+    install_requires=["requests>=2.31.0",
+                      "termcolor>=2.3.0"],
     entry_points={
         "console_scripts": [
             "play-notion = main:main",
